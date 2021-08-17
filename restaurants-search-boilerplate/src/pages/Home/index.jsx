251a0141cac4dc2a9092dee5fcf7f9035ce1f4cf
pldnz <paulosouzadiniz@hotmail.com>
2021-08-16 22:13:29 -0300

    iniciada a criação da barra lateral

--- restaurants-search-boilerplate/src/pages/Home/index.jsx
+++ restaurants-search-boilerplate/src/pages/Home/index.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TextField, {Input} from '@material/react-text-field/';
 
-import { Container } from './styles';
+import logo from '../../assets/logo.svg'
 
-const Home = () => <Container>Hello World</Container>;
+import { Container, Search } from './styles';
+
+const Home = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  return(
+    <Container>
+      <Search>
+        <img src={logo} alt="logo do buscador de restaurantes" />
+        <TextField
+          label='Pesquisar'
+          // outlined
+          // onTrailingIconSelect={()=> this.setState({value: ''})}
+          // trailingIcon={<MaterialIcon role ="button" icon="delete"/>}
+        ><Input 
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        </TextField>
+      </Search>
+    </Container>
+
+  ); 
+};
 
 export default Home;
