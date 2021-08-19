b89ed51566c74189cd7bb97ee91f83f2db211222
pldnz <paulosouzadiniz@hotmail.com>
2021-08-18 22:08:10 -0300

    iniciada a implementação do carousel

--- restaurants-search-boilerplate/src/pages/Home/index.jsx
+++ restaurants-search-boilerplate/src/pages/Home/index.jsx
@@ -1,30 +1,33 @@
 import React, { useState } from 'react';
 import TextField, {Input} from '@material/react-text-field/';
+import MaterialIcon from '@material/react-material-icon';
 
 import logo from '../../assets/logo.svg'
 
-import { Container, Search } from './styles';
+import { Wrapper, Container, Search, Logo, Map } from './styles';
 
 const Home = () => {
   const [inputValue, setInputValue] = useState('');
 
   return(
-    <Container>
-      <Search>
-        <img src={logo} alt="logo do buscador de restaurantes" />
-        <TextField
-          label='Pesquisar'
-          // outlined
-          // onTrailingIconSelect={()=> this.setState({value: ''})}
-          // trailingIcon={<MaterialIcon role ="button" icon="delete"/>}
-        ><Input 
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
-        </TextField>
-      </Search>
-    </Container>
-
+    <Wrapper>
+      <Container>
+        <Search>
+          <Logo src={logo} alt="logo do buscador de restaurantes" />
+          <TextField
+            label='Pesquisar'
+            // outlined
+            // onTrailingIconSelect={()=> this.setState({value: ''})}
+            trailingIcon={<MaterialIcon role ="button" icon="search"/>}
+          ><Input 
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          </TextField>
+        </Search>
+      </Container>
+      <Map />
+    </Wrapper>
   ); 
 };
 
